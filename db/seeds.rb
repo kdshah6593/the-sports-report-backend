cef2a825b75f1eed01481144c6787b49352ba392
Kunal <kdshah6593@gmail.com>
2021-04-01 19:00:05 -0400

    set up users in database and api version

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+
+kunal = User.create(username: 'kunals', password: 'password', email: 'kunals@email.com', first_name: 'Kunal', last_name: 'Shah')
