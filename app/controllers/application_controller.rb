baba0b813fce8c4af199ca2a175823277297fc38
Kunal <kdshah6593@gmail.com>
2021-04-02 11:28:22 -0400

    hard code current user for front end testing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::API
+
+    #temporary hard code current user for front end; this will change when JWT AUTH is added
+    def current_user
+        User.first
+    end
+
+    def logged_in?
+        !!current_user
+    end
 end
