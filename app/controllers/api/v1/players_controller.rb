ba8e8995a351fef38e9a2e93882adb780bf78ab7
Kunal <kdshah6593@gmail.com>
2021-04-05 15:30:36 -0400

    add new player to user

--- app/controllers/api/v1/players_controller.rb
+++ app/controllers/api/v1/players_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::PlayersController < ApplicationController
-  # before_action will need the current user one here
+  before_action :current_user
 
   # GET /players
   def index
@@ -14,23 +14,20 @@ class Api::V1::PlayersController < ApplicationController
 
   # POST /players #this should use the current user build association to make new players
   def create
-    @player = Player.new(player_params)
-
-    if @player.save
-      render json: @player, status: :created, location: @player
-    else
-      render json: @player.errors, status: :unprocessable_entity
-    end
+    player = Player.find_or_create_by(sportsDBId: player_params["sportsDBId"])
+    player.update(player_params) # this will update any players find with new information from the api if team switched
+    current_user.players.push(player)
+    render json: UserSerializer.new(current_user)
   end
 
   # PATCH/PUT /players/1
-  def update
-    if @player.update(player_params)
-      render json: @player
-    else
-      render json: @player.errors, status: :unprocessable_entity
-    end
-  end
+  # def update
+  #   if @player.update(player_params)
+  #     render json: @player
+  #   else
+  #     render json: @player.errors, status: :unprocessable_entity
+  #   end
+  # end
 
   # # DELETE /players/1
   # def destroy
