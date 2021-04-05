5bb8a12d42440294f86388539edae8ce6cd123f8
Kunal <kdshah6593@gmail.com>
2021-04-05 16:26:58 -0400

    handle post request new fav team for user

--- app/controllers/api/v1/teams_controller.rb
+++ app/controllers/api/v1/teams_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::TeamsController < ApplicationController
-  # before_action will need current user
+  before_action :current_user
 
   # GET /teams
   def index
@@ -15,23 +15,20 @@ class Api::V1::TeamsController < ApplicationController
 
   # POST /teams #this should use the current user build association to make new teams
   def create
-    @team = Team.new(team_params)
-
-    if @team.save
-      render json: @team, status: :created, location: @team
-    else
-      render json: @team.errors, status: :unprocessable_entity
-    end
+    team = Team.find_or_create_by(sportsDBId: team_params["sportsDBId"])
+    team.update(team_params)
+    current_user.teams.push(team)
+    render json: UserSerializer.new(current_user)
   end
 
-  # PATCH/PUT /teams/1
-  def update
-    if @team.update(team_params)
-      render json: @team
-    else
-      render json: @team.errors, status: :unprocessable_entity
-    end
-  end
+  # # PATCH/PUT /teams/1
+  # def update
+  #   if @team.update(team_params)
+  #     render json: @team
+  #   else
+  #     render json: @team.errors, status: :unprocessable_entity
+  #   end
+  # end
 
   # # DELETE /teams/1
   # def destroy
