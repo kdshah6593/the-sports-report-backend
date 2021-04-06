c510e663d2fb262a8452c6838f8b079b4ba1d89e
Kunal <kdshah6593@gmail.com>
2021-04-06 17:20:06 -0400

    added login post route

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
       resources :users
       resources :teams
       resources :players
+      post '/login', to: 'auth#create'
     end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
