8d87c515fa93992dd24da53ec1ebb73f2f03721a
Kunal <kdshah6593@gmail.com>
2021-04-06 17:22:39 -0400

    add helper method, update controller action for new user created

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,41 +1,26 @@
 class Api::V1::UsersController < ApplicationController
+  skip_before_action :authorized
   before_action :set_user, only: [:show, :update, :destroy]
 
-  # GET /users
   def index
     @users = User.all
     render json: UserSerializer.new(@users)
   end
 
-  # GET /users/1
   def show
     user = User.find_by(id: params[:id])
     render json: UserSerializer.new(@user)
   end
 
-  # POST /users
   def create
-    @user = User.new(user_params)
+    @user = User.create(user_params)
 
-    if @user.save
-      render json: @user, status: :created, location: @user
+    if @user.valid?
+      @token = encode_token(user_id: @user.id)
+      render json: { user: UserSerializer.new(@user), jwt: @token }, status: :created
     else
-      render json: @user.errors, status: :unprocessable_entity
+      render json: {errors: @user.errors.full_messages}, status: :not_acceptable
     end
-  end
-
-  # PATCH/PUT /users/1
-  def update
-    if @user.update(user_params)
-      render json: @user
-    else
-      render json: @user.errors, status: :unprocessable_entity
-    end
-  end
-
-  # DELETE /users/1
-  def destroy
-    @user.destroy
   end
 
   private
